437f2179c5284211d692405404f9a7c4c65dfe3b
jaymin1088 <jaymin_10_88@yahoo.co.in>
2024-02-13 01:07:46 +0530

    React Vite 3
    Update react & hook

--- React_example/ReactPropsApp3/src/components/Header.jsx
+++ React_example/ReactPropsApp3/src/components/Header.jsx
@@ -2,7 +2,9 @@ import reactIMG from "../assets/react.png";
 import { CORE_CONCEPTS } from "../Data.js";
 import CoreConcept from "./CoreConcept.jsx";
 import React from "react";
+import { useState } from "react";
 import TabButton from "./TabButton.jsx";
+import { EXAMPLES } from "../Data.js";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -11,6 +13,11 @@ function RandomInt(max) {
 }
 
 export default function Header() {
+  const [selectedTopic, setSelectedTopic] = useState(null);
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
   const description = reactDescriptions[RandomInt(2)];
   console.log(description);
   return (
@@ -36,15 +43,51 @@ export default function Header() {
           </ul>
         </section>
         <section id="examples">
-          <h2 className="text-4xl mt-9"> Examples </h2>
-          <menu className="flex justify-centerk ml-10">
-            <TabButton>Component</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>Event</TabButton>
+          <h2 className="text-5xl mt-9"> Examples </h2>
+          <menu className="flex justify-center mt-7">
+            <TabButton onSelect={() => handleSelect("components")}>
+              Component
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
+          {/* {!selectedTopic ? (                                           // Using Ternary Operator
+            <p className="text-1xl mt-8">Please Select a Topic</p>
+          ) : (
+            <div id="tab-content" className="text-1xl mt-8 ml-28 text-left">
+              {EXAMPLES[selectedTopic] ? (
+                <>
+                  <h3>{EXAMPLES[selectedTopic].title}</h3>
+                  <p>{EXAMPLES[selectedTopic].description}</p>
+                  <pre>
+                    <code>{EXAMPLES[selectedTopic].code}</code>
+                  </pre>
+                </>
+              ) : (
+                <p className="text-1xl ml-8">
+                  Example not found for selected topic
+                </p>
+              )}
+            </div>
+          )} */}
+
+          {!selectedTopic && (                                        // Using Logical Operator
+            <p className="text-1xl mt-8">Please Select a Topic</p>
+          )}
+          {selectedTopic && (
+            <div id="tab-content" className="text-1xl mt-8 ml-28 text-left">
+              <>
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                  <code>{EXAMPLES[selectedTopic].code}</code>
+                </pre>
+              </>
+            </div>
+          )}
         </section>
       </div>
     </header>
   );
-  }
+}
--- React_example/ReactPropsApp3/src/components/TabButton.jsx
+++ React_example/ReactPropsApp3/src/components/TabButton.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function TabButton({children}) {
+export default function TabButton({children,onSelect}) {
   return (
     <li>
-      <button className="mt-6 ml-9 bg-indigo-800 rounded-lg	">{children}</button>
+      <button className="mt-6 ml-9 bg-indigo-800 rounded-lg hover:bg-indigo-900 transition-colors duration-300 ease-in-out" onClick={onSelect}>{children}</button>
     </li>
   );
 }
