3ffeda7b2ade39ed1fcd09eccb6fc5f012bb3bfe
jaymin1088 <jaymin_10_88@yahoo.co.in>
2024-03-04 23:27:44 +0530

    node

--- REACT_KGCoding/Node/examples/2/app.js
+++ REACT_KGCoding/Node/examples/2/app.js
@@ -1,5 +1,6 @@
 const http = require("http");
-const fs=require('fs')
+const fs = require("fs");
+const { error } = require("console");
 
 const server = http.createServer((req, res) => {
   //Keep on running as long as there are event listeners registered
@@ -7,7 +8,7 @@ const server = http.createServer((req, res) => {
   //console.log(req.url,req.method,req.headers);
   // process.exit();
   const url = req.url;
-  const method=req.method;
+  const method = req.method;
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title> Node || First Page </title></head>");
@@ -17,12 +18,28 @@ const server = http.createServer((req, res) => {
     res.write("</html>");
     return res.end();
   }
-  if(url==="/message" && method==="POST"){
-        fs.writeFileSync("Message.txt","Text Added Successfully");
-        res.statusCode=302;
-        res.setHeader=('Location','/');
-        return res.end();
-    }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+        const parseBody=Buffer.concat(body).toString();
+        const message=parseBody.split('=')[1];
+        fs.writeFileSync('message.txt',message,(error)=>{
+            res.statusCode = 302;
+            res.setHeader = ("Location", "/");
+            return res.end();
+
+        });
+        //console.log(parseBody);
+    });
+    //fs.writeFileSync("Message.txt", "Text Added Successfully");
+   /*  res.statusCode = 302;
+    res.setHeader = ("Location", "/");
+    return res.end(); */
+  }
   res.setHeader("content-type", "text/html");
   res.write("<html>");
   res.write("<head><title> Node || First Page </title></head>");
