fd14f36ac052f592df4ce438cb64a8db4ddb1462
jaymin1088 <jaymin_10_88@yahoo.co.in>
2024-04-01 22:57:17 +0530

    Express Template Engine
    layout and without layout

---
+++ Practice_Examples/Express_revised/3_Express_TmplteEngine/app.js
@@ -0,0 +1,30 @@
+const path=require('path');
+const http = require("http");
+const bodyParser = require("body-parser");
+const express = require("express");
+
+const adminData = require("./routes/admin.js");
+const usrRoutes = require("./routes/addusr.js");
+
+const app = express();
+app.set('view engine','pug');
+app.set('views','views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'public','')));
+
+
+  
+app.use('/admin',adminData.routes);
+app.use(usrRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404err',{pageError:'404 || Error'});
+    
+  }); 
+
+const server = http.createServer(app);
+const port=3000
+app.listen(port, () => {
+  console.log(`Server run on ${port}`);
+});
