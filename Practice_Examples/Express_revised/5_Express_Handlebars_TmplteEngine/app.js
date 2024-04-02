282307526cfc69188045f2619939e3821bd80937
jaymin1088 <jaymin_10_88@yahoo.co.in>
2024-04-02 18:22:01 +0530

    Express Node JS pug
    Template layout

---
+++ Practice_Examples/Express_revised/5_Express_Handlebars_TmplteEngine/app.js
@@ -0,0 +1,34 @@
+const path=require('path');
+const http = require("http");
+const bodyParser = require("body-parser");
+const express = require("express");
+const expressHbs = require("express-handlebars");
+
+const adminData = require("./routes/admin.js");
+const usrRoutes = require("./routes/addusr.js");
+
+const app = express();
+
+app.engine('hbs',expressHbs);
+app.set('view engine','hbs');
+app.set('views','views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'public','')));
+
+
+  
+app.use('/admin',adminData.routes);
+app.use(usrRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404err',{pageTitle:'404 || Error'});
+    
+  }); 
+
+
+const server = http.createServer(app);
+const port=3000
+app.listen(port, () => {
+  console.log(`Server run on ${port}`);
+});
