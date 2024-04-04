f1bdf7806e19e9d6a28e1bd0e15ed67848e26feb
jaymin1088 <jaymin_10_88@yahoo.co.in>
2024-04-04 07:17:08 +0530

    Node EJS Express JS
    Template and non template EJS

---
+++ Practice_Examples/Express_revised/8_Express_EJS_TmplteEngine_layout/app.js
@@ -0,0 +1,33 @@
+const path=require('path');
+const http = require("http");
+const bodyParser = require("body-parser");
+
+const express = require("express");
+
+const adminData = require("./routes/admin.js");
+const usrRoutes = require("./routes/addusr.js");
+
+const app = express();
+
+app.set('view engine','ejs');
+app.set('views','views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'public','')));
+
+
+  
+app.use('/admin',adminData.routes);
+app.use(usrRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404err',{pageTitle:'404 || Error',path:req.url});
+    
+  }); 
+
+
+const server = http.createServer(app);
+const port=3001
+app.listen(port, () => {
+  console.log(`Server run on ${port}`);
+});
